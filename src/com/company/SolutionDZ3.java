a598829d543e5e7a69b5841e89f35550d946389e
Nick <vorobyev.nikitos@gmail.com>
2022-12-01 14:54:31 +0300

    дз выполнено

--- src/com/company/SolutionDZ3.java
+++ src/com/company/SolutionDZ3.java
@@ -7,12 +7,11 @@ public class SolutionDZ3 {
         final int endOfPeriod = currentYear + 100;
         int year = beginOfPeriod;
 
-         while ((year + 79) <= endOfPeriod) {
-             year += 79;
-             System.out.println(year);
+        while ((year + 79) <= endOfPeriod) {
+            year += 79;
+            System.out.println(year);
         }
     }
-
     public static void task2() {
         for (int i = 1; i <= 10; i++) {
             System.out.println("2 * " + i + " = " + (2 * i));
